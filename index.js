faa11efe8338addfbf714bf28d08112a0a559387
Stanislaw Mruczkiewicz <stasmruko@gmail.com>
2025-10-06 07:28:04 +0000

    added few things

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-import http from "node:https"
-import fs from "node:fs"
+import { createServer } from "node:https";
+import { readFileSync } from "node:fs";
+
+const port = 8000;
+const host = localhost;
+
+const index_html = readFileSync(pzaw-projekty-stasmru/index.html);
+const favicon_ico = readFileSync(pzaw-projekty-stasmru/favicon.ico);
 
